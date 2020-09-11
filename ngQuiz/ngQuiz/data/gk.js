8735d05ff78d8981e0f717e7768e5988126fc40e
Vardhanladdu <Vardhan.gattu24@gmail.com>
2020-09-11 21:15:07 +0530

    phase 4

---
+++ ngQuiz/ngQuiz/data/gk.js
@@ -0,0 +1,117 @@
+{
+    "quiz": {
+        "Id": 1,
+        "name": "General knowledge ",
+        "description": "General kowledge (contains sports,general etc.)"
+    },
+    "questions": [{ 
+        "Id": 1010,
+        "Name": "Who is the first prime minister of India?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "B R ambedkar", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Nehru", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Rajendra prasad", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1011,
+        "Name": "The most frequent female astronaut in space is -", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Shannon Lucid", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Kalpana Chawla", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Sunita Williams", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1012,
+        "Name": " Which country has the highest production of cotton?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "America", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "India", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "China", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Egypt", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1013,
+        "Name": "Who built the Red Fort in Agra?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Akbar", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Aurangzeb", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Shah Jahan", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1014,
+        "Name": "Where are the Ajanta caves located?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Odisaa", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "MP", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "UP", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Maharastra", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1015,
+        "Name": "Melbourne's cricket stadium is in which country?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "South Africa", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "India", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Australia", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1016,
+        "Name": "Where is Indira Point located?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "lakshadeep", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Andaman and Nicobar", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "daman and diu", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "none of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1017,
+        "Name": "Where is Diamond Harbor and Salt Lake-City located?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Mumbai", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Kolkata", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Chennai", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Bangalore", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1018,
+        "Name": "Silent Valley is in which state?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Mp", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Ap", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Kerala", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Up", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1019,
+        "Name": "When did Swami Vivekananda establish the Ramakrishna Mission?",
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "1897", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "1987", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "1899", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "1888", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    }]
+}
---
+++ ngQuiz/ngQuiz/data/pm.js
@@ -0,0 +1,117 @@
+{
+    "quiz": {
+        "Id": 3,
+        "name": "Politics and movies",
+        "description": "Politics in india"
+    },
+    "questions": [{
+        "Id": 1010,
+        "Name": "Who proposed the Preamble to the Constitution before the Constituent Assembly?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Subhash Chandra Bose", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": " Pt. Jawaharlal Nehru", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Gandhi", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Ambedkar", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1011,
+        "Name": "Who among the following became the President of India twice in a row?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Rajendra Prasad", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "R. Venkatman", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "V. V. Giri", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": " Radhakrishnan", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1012,
+        "Name": "  When was Goa formed a part of the Indian Republic?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "1961", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "1948", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "1952", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "1953", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1013,
+        "Name": " Which of the following are known as Gandhi?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Khan Abdul Ghaffar Khan", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": " Lord Mountbatten", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Sardar Patel", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1014,
+        "Name": "The policy of 'divide and rule' was adopted?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Lord Mountbatten", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Lord William Bentinck", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Lord Mountbatten", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Lord Curzon", "IsAnswer": true }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1015,
+        "Name": "Fisrt Indian Movie Submitted for Ocsar?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Madhumati", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "The Guide", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Mother India", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Amrapali", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1016,
+        "Name": "Who among the following invented the cinematograph?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Mani Sethna", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Lumiere Brothers", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Dada Saheb Phalke", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Dhirendra Nath Ganguly", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1017,
+        "Name": " Which was the first India's talkie film?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Pundlik", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Alam Ara", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Raja Harishchandra", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": " Nal Damayanti", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1018,
+        "Name": "Which of the following was the first made indigenous, coloured film of India?", 
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Kisan Kanya", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Alama Ara", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Dev das", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Pather Panchali", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    },
+    {
+        "Id": 1019,
+        "Name": "Which of the following regional cinema referred to as tollywood?:",
+        "QuestionTypeId": 1,
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "Telugu Cinema", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Tamil Cinema", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Malayalam Cinema", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Marathi Cinema", "IsAnswer": false }],
+        "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
+    }]
+}
